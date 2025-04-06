1b91ecd27f98a5064a0fd13cac10c1d25f617204
Alexander van der Leek <Alexander.VanderLeek@bbd.co.za>
2025-04-06 13:32:20 +0200

    Add interfaces for user repository and user service.

--- API/src/StellarPath.API/StellarPath.API.Core/Interfaces/Services/IUserService.cs
+++ API/src/StellarPath.API/StellarPath.API.Core/Interfaces/Services/IUserService.cs
@@ -1,8 +1,12 @@
-﻿namespace StellarPath.API.Core.Interfaces.Services;
+﻿using StellarPath.API.Core.DTOs;
+
+namespace StellarPath.API.Core.Interfaces.Services;
 internal interface IUserService
 {
     Task<bool> UserExistsAsync(string googleId);
     Task<int> CreateUserAsync(string googleId, string email, string firstName, string lastName);
-    
+    Task<UserDto> GetUserByGoogleIdAsync(string googleId);
+    Task<string> GetUserRoleAsync(string googleId);
+
 }
 
