a162b296d4d0161d10892e2cd7e39d48772b4b2a
Leshego Talane <repo.talane@bbd.co.za>
2025-04-09 16:54:04 +0200

    changing of repo in the policy to FrancoduBuisson/StellarPath

--- terraform/oidc-role.tf
+++ terraform/oidc-role.tf
@@ -23,7 +23,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           },
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:LeshegoT/*:*"
+            "token.actions.githubusercontent.com:sub" = "repo:FrancoduBuisson/StellarPath:*"
           }
         }
       }
