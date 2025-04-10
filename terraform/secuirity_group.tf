c4b3809f15b495d30b558f64d9e25fed16b46f9a
Leshego Talane <repo.talane@bbd.co.za>
2025-04-10 14:32:18 +0200

    added logs and changes to security

--- terraform/secuirity_group.tf
+++ terraform/secuirity_group.tf
@@ -1,10 +1,12 @@
 resource "aws_security_group" "ecs_sg" {
-  vpc_id = aws_vpc.main.id
+  name        = "${var.app_name}-ecs-sg"
+  description = "Allow all traffic to ECS from all networks"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -17,7 +19,9 @@ resource "aws_security_group" "ecs_sg" {
 }
 
 resource "aws_security_group" "rds_sg" {
-  vpc_id = aws_vpc.main.id
+  name        = "${var.app_name}-rds-sg"
+  description = "Allow database access from all networks"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 5432
@@ -32,4 +36,40 @@ resource "aws_security_group" "rds_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.app_name}-alb-sg"
+  description = "Allow public HTTP/HTTPS access"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "ecs_from_alb" {
+  security_group_id        = aws_security_group.ecs_sg.id
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.alb_sg.id
 }
