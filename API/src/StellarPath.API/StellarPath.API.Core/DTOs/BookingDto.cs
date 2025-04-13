02a5e7e1bb2520cf7e2eb8e28065e9776838b949
Alexander van der Leek <Alexander.VanderLeek@bbd.co.za>
2025-04-13 12:31:25 +0200

    Add models and initial interfaces for booking realted stuff.

---
+++ API/src/StellarPath.API/StellarPath.API.Core/DTOs/BookingDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarPath.API.Core.DTOs;
+
+public class BookingDto
+{
+    public int BookingId { get; set; }
+    public string GoogleId { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public int CruiseId { get; set; }
+    public string DepartureDestination { get; set; } = string.Empty;
+    public string ArrivalDestination { get; set; } = string.Empty;
+    public DateTime LocalDepartureTime { get; set; }
+    public int SeatNumber { get; set; }
+    public DateTime BookingDate { get; set; }
+    public DateTime BookingExpiration { get; set; }
+    public int BookingStatusId { get; set; }
+    public string BookingStatusName { get; set; } = string.Empty;
+}
